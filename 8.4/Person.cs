ad2bcc8986ad3d777a21463dcc8a8eb0f14ed143
Mihael Kardash <mixan193@gmail.com>
2023-07-24 19:42:06 +0300

    Добавьте файлы проекта.

---
+++ 8.4/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _8._4
+{
+    internal class Person
+    {
+        public string name;
+        public string street;
+        public string houseNumber;
+        public string flatNumber;
+        public string mobilePhone;
+        public string flatPhone;
+        public Person(string name, string street, string houseNumber, string flatNumber, string mobilePhone, string flatPhone)
+        {
+            this.name = name;
+            this.street = street;
+            this.houseNumber = houseNumber;
+            this.flatNumber = flatNumber;
+            this.mobilePhone = mobilePhone;
+            this.flatPhone = flatPhone;
+        }
+
+        public XElement SerializePerson()
+        {
+            XElement person = new XElement("Person");
+            XAttribute personAttribute = new XAttribute("name", name);
+            XElement address = new XElement("Address");
+            XElement street = new XElement("Street");
+            street.Value = this.street;
+            XElement houseNumber = new XElement("HouseNumber");
+            houseNumber.Value = this.houseNumber;
+            XElement flatNumber = new XElement("FlatNumber");
+            flatNumber.Value = this.flatNumber;
+            XElement phones = new XElement("Phones");
+            XElement mobilePhone = new XElement("MobilePhone");
+            mobilePhone.Value = this.mobilePhone;
+            XElement flatPhone = new XElement("FlatPhone");
+            flatPhone.Value = this.flatPhone;
+            address.Add(street, houseNumber, flatNumber);
+            phones.Add(mobilePhone, flatPhone);
+            person.Add(personAttribute, address, phones);
+            return person;
+        }
+    }
+}
---
+++ 8.4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _8._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.GetEncoding("utf-16");
+            XDocument xDocument = new XDocument();
+            Person p;
+            string name;
+            string street;
+            string houseNumber;
+            string flatNumber;
+            string mobilePhone;
+            string flatPhone;
+
+            while (true)
+            {
+                Console.WriteLine("Введите имя");
+                name = Console.ReadLine();
+                Console.WriteLine("Введите улицу");
+                street = Console.ReadLine();
+                Console.WriteLine("Введите номер дома");
+                houseNumber = Console.ReadLine();
+                Console.WriteLine("Введите номер квартиры");
+                flatNumber = Console.ReadLine();
+                Console.WriteLine("Введите мобильный номер");
+                mobilePhone = Console.ReadLine();
+                Console.WriteLine("Введите домашний номер");
+                flatPhone = Console.ReadLine();
+                xDocument.Add(new Person(name, street, houseNumber, flatNumber, mobilePhone, flatPhone).SerializePerson());
+                Console.WriteLine("Желаете продолжить? Y/N");
+                char temp = Console.ReadKey().KeyChar;
+                if (temp == 'n' || temp == 'N' || temp == 'т' || temp == 'Т')
+                {
+                    break;
+                }
+            }
+            xDocument.Save("Book.xml");
+            Console.ReadKey();
+        }
+    }
+}
